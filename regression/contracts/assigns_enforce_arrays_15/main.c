d860d47a19ef07304d44fd41c8ec5704c91cee46
Christopher Wagner <cwwgner@amazon.com>
2020-07-27 14:49:10 -0400

    Added rest of array regression tests.

---
+++ regression/contracts/assigns_enforce_arrays_15/main.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+
+void assigns_ptr(int *x) 
+__CPROVER_assigns(*x)
+{
+  *x = 200;
+}
+
+void assigns_range(int a[], int len) 
+__CPROVER_assigns(a[2,5])
+{
+  int *ptr = &(a[3]);
+  assigns_ptr(ptr);
+}
+
+int main()
+{
+  int arr[10];
+  assigns_range(arr, 10);
+
+  return 0;
+}
